3bfef85e0078f8c08ff8d3c806113d03367297ce
Adeeba-Naz <justcreated.01@gmail.com>
2024-09-03 00:23:47 +0530

    Comparison Operator

---
+++ second.java
@@ -0,0 +1,11 @@
+public class second {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 9;
+
+        if(a>b)
+        System.out.println(a);
+        else
+        System.out.println(b);
+    }
+}
